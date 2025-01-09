2c39d10b08a78d01326cae67e2a27e5677af1a19
Frederic Hoerni <fhoerni@free.fr>
2025-01-09 21:38:42 +0100

    xeert.cpp: parse dummy cmd line arguments

--- src/xeert.cpp
+++ src/xeert.cpp
@@ -4,8 +4,80 @@
 
 #include "config.h"
 
+const char *argp_program_version = PACKAGE_VERSION;
+static char doc[] = "xeert --  a tool to display information about certificates";
+static char args_doc[] = "CERTIFICATE ...";
+static struct argp_option options[] = {
+	{"verbose",  'v', 0,      0,  "Produce verbose output" },
+	{"quiet",    'q', 0,      0,  "Don't produce any output" },
+	{"silent",   's', 0,      OPTION_ALIAS },
+	{"output",   'o', "FILE", 0,  "Output to FILE instead of standard output" },
+	{ 0 }
+};
+
+struct arguments
+{
+	char *args[2];  /* arg1 & arg2 */
+	int silent, verbose;
+	char *output_file;
+};
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+	struct arguments *arguments =(struct arguments *) state->input;
+
+	switch (key)
+		{
+		case 'q': case 's':
+			arguments->silent = 1;
+			break;
+		case 'v':
+			arguments->verbose = 1;
+			break;
+		case 'o':
+			arguments->output_file = arg;
+			break;
+		
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 2)
+				argp_usage(state); /* Too many arguments. */
+		
+			arguments->args[state->arg_num] = arg;
+		
+			break;
+		
+		case ARGP_KEY_END:
+			if (state->arg_num < 2)
+				argp_usage(state); /* Not enough arguments. */
+			break;
+		
+		default:
+			return ARGP_ERR_UNKNOWN;
+		}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+
 int main(int argc, char **argv)
 {
-	
-	printf("hello\n");
+	struct arguments arguments;
+	/* Default values. */
+	arguments.silent = 0;
+	arguments.verbose = 0;
+	arguments.output_file = "-";
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	printf("ARG1 = %s\nARG2 = %s\nOUTPUT_FILE = %s\n"
+	      "VERBOSE = %s\nSILENT = %s\n",
+	       arguments.args[0], arguments.args[1],
+	       arguments.output_file,
+	       arguments.verbose ? "yes" : "no",
+	       arguments.silent ? "yes" : "no");
+
+	return 0;
+
 }
+
